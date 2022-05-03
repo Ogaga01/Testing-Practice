4f9b3e58d33f18094a956c9d29949592fad85185
Ogaga <iyaraogaga69@gmail.com>
2022-05-03 13:39:23 +0100

    Add Third Task

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-function reverseString(string){
-    let splitString = string.split('');
-    let reverseArray = splitString.reverse();
-    let newString = reverseArray.join('');
-    return newString;
+const Calculator = {
+    constructor(a,b)
+    {this.a = a;
+    this.b = b;
+},
+ addition(a,b){
+    return a + b;
+},
+subtraction(a,b){
+    return a - b;
+},
+multiplication(a,b){
+    return a * b;
+},
+division(a,b){
+    return a/b;
 }
-module.exports = reverseString
+}
+
+module.exports = Calculator;
--- index.test.js
+++ index.test.js
@@ -1,5 +1,17 @@
-const reverseString = require('./index')
+const { addition, subtraction, multiplication, division } = require('./index')
+const Calculator = require('./index')
 
-test('reversed string', () => {
-    expect(reverseString('break')).toBe('kaerb')
+describe('calculator', () => {
+    test('addition', () => {
+        expect(addition(100,20)).toEqual(120)
+    });
+    test('subtraction', () => {
+        expect(subtraction(200,100)).toEqual(100)
+    });
+    test('multiplication', () => {
+        expect(multiplication(10,20)).toEqual(200)
+    });
+    test('division', () => {
+        expect(division(700,10)).toEqual(70)
+    });
 })
